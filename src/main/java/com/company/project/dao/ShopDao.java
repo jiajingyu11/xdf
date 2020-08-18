4f498ec40fcbc0e3f7cc76f9d01f6fbf036ba081
jiajingyu <jingyu.jia01@mljr.com>
2020-08-18 17:42:21 +0800

    Spring Boot API Project Seed 是一个基于Spring Boot & MyBatis的种子项目，用于快速构建中小型API、RESTful API项目，该种子项目已经有过多个真实项目的实践，稳定、简单、快速，使我们摆脱那些重复劳动，专注于业务代码的编写，减少加班。下面是一个简单的使用演示，看如何基于本项目在短短几十秒钟内实现一套简单的API，并运行提供服务。

---
+++ src/main/java/com/company/project/dao/ShopDao.java
@@ -0,0 +1,14 @@
+package com.company.project.dao;
+
+import com.company.project.model.Shop;
+import org.apache.ibatis.annotations.Param;
+
+
+/**
+ * @Auther: jiajingyu
+ * @Date: 2020/8/18 11:26
+ * @Description:
+ */
+public interface ShopDao extends ShopMapper<Shop> {
+    Shop queryShop(@Param("id") Integer id);
+}
--- src/test/java/CodeGenerator.java
+++ src/test/java/CodeGenerator.java
@@ -37,8 +37,8 @@ public class CodeGenerator {
     private static final String DATE = new SimpleDateFormat("yyyy/MM/dd").format(new Date());//@date
 
     public static void main(String[] args) {
-//        genCode("t_shop");
-        genCodeByCustomModelName("t_shop","Shop");
+        genCode("t_shop");
+        //genCodeByCustomModelName("t_shop","Shop");
     }
 
     /**
